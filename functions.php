9163270c3ac8d1c08eba00a95cce93b3714113ef
mdsagor <mdsagorahmed1224@gmail.com>
2023-11-11 17:26:22 +0600

    part9

--- functions.php
+++ functions.php
@@ -75,3 +75,13 @@ add_action('customize_register','hallo_customizar_register');
 // Menu Register
 register_nav_menu('main_menu',__('Main Menu','hallocoder'));
 
+function hallo_nav_description($item_output, $item,$args){
+    if (!empty ($item->description)) {
+        $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' .
+        $item->description . '</span>' . $args->link_after . '</a>', $item_output);
+    }
+
+    return $item_output;
+}
+add_filter('walker_nav_menu_start_el','hallo_nav_description', 10, 3);
+
