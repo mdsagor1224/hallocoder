298cd4e8bfaa727d0258d7d30e9f308aa26acbeb
mdsagor <mdsagorahmed1224@gmail.com>
2023-11-12 22:14:18 +0600

    part15

--- inc/default.php
+++ inc/default.php
@@ -7,3 +7,16 @@ add_theme_support('title-tag');
 
 add_theme_support('post-thumbnails', array('page','post'));
 add_image_size('post-thumbnails', 970, 350, true);
+
+
+// Excerpt to 40 word
+function hallo_excerpt_more($more){
+    return '<br> <br> <a class="readmore" href="'.get_permalink($post->ID) . '">' . 'Read More' . '</a>';
+}
+
+add_filter('excerpt_more', 'hallo_excerpt_more');
+
+function hallo_excerpt_lenght($length){
+    return 40;
+}
+add_filter('excerpt_length','hallo_excerpt_lenght', 999);
