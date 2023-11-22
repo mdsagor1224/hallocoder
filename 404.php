37bbf8b46a42408e63dcf0dec57aa149ae419612
mdsagor <mdsagorahmed1224@gmail.com>
2023-11-22 11:00:03 +0600

    part 22

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+/*
+* The Template Displaying 404 page  (note found)
+*/
+ get_header();
+ 
+ ?>
+
+    <section id="body_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 error_page">
+                  <p>404 Error - Page not Found</p>
+                  <h1>Oops! Looks Like somethings was wrong</h1>
+                  <div class="error_search">
+                    <?php get_search_form(); ?>
+                  </div>
+
+                  <a href="<?php echo home_url(); ?>" class="homepage">Homepage</a>
+                </div>
+             </div>
+        </div>
+    </section>
+
+<?php get_footer();?>
