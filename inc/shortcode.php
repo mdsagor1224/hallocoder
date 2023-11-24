b69a33350513be9ea4a916ff46a79618913cbc6f
mdsagor <mdsagorahmed1224@gmail.com>
2023-11-24 19:52:14 +0600

    part26

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -6,7 +6,7 @@ function basic_shortcode(){
 }
 add_shortcode('sagor','basic_shortcode');
 
-
+// button shortcode
 function button_shortcode($atts, $content = null){
     $values = shortcode_atts(array(
         'url' => '#'
@@ -16,3 +16,50 @@ function button_shortcode($atts, $content = null){
 }
 
 add_shortcode('button','button_shortcode');
+
+
+// shortcode and custom post
+
+function service_shortcode($atts){
+    ob_start();
+    
+    $query = new WP_Query(array(
+        'post_type' => 'service',
+        'posts_per_page' => 3,
+        'order' => 'ABC',
+        'orderby' => 'title'
+    ));
+
+    if ($query -> have_posts()) {?>
+
+        <section id="service_area">
+            <div class="container">
+                <div class="row">
+
+
+                <?php 
+                    while($query->have_posts() ) : $query->the_post();
+                ?>
+                    <div class="col-md-4">
+                        <div class="child_service">
+                            <h2><?php the_title();?></h2>
+                            <?php echo the_post_thumbnail('service');?>
+                            <?php the_excerpt();?>
+                            
+                        </div>
+                    </div>
+
+
+                    <?php 
+                        endwhile; wp_reset_postdata();
+                    ?>
+               </div>
+            </div>
+        </section>
+
+        <?php $myvariable = ob_get_clean();
+        return $myvariable;
+    }
+}
+
+add_shortcode('service','service_shortcode');
