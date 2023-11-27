e4ccf14a9c6e5d389ed4fb0d67ffffe2db79018a
mdsagor <mdsagorahmed1224@gmail.com>
2023-11-27 10:48:35 +0600

    part33

--- inc/custom_post.php
+++ inc/custom_post.php
@@ -55,11 +55,28 @@ function custom_service(){
             'hierarchial' => true,
             'show_ui' => true,
             'capability_type' => 'post',
+            'taxonomies' => array('category'),
             'rewrite' => array('slug' => 'service'),
             'supports' => array('title','thumbnail','editor','excerpt'),
-        )
+         )
         );
 }
 
 
 add_action('init','custom_service');
+
+
+function query_post_type($query){
+    if (is_category()){
+        $post_type = get_query_var('post_type');
+        if ($post_type) {
+            $post_type = $post_type;
+        }else {
+            $post_type = array('post','service');
+            $query->set('post_type', $post_type);
+            return $query;
+        }
+    }
+}
+
+add_filter('pre_get_posts','query_post_type');
